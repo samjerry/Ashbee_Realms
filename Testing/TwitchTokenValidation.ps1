5f4b24b49b26416315e82288d6e235f0000db151
MarrowOfAlbion <jerry.sam@ziggo.nl>
2025-12-15 23:12:35 +0100

    Remove Testing folder from repository and add to gitignore

--- Testing/TwitchTokenValidation.ps1
+++
@@ -1,8 +0,0 @@
-# Read token from .env and strip optional oauth: prefix
-$envFile = Get-Content .env
-$tokLine = $envFile | Where-Object { $_ -match '^BOT_OAUTH_TOKEN=' }
-$tok = $tokLine -replace '^BOT_OAUTH_TOKEN=','' -replace '"',''
-$tokClean = $tok -replace '^oauth:',''
-
-# Validate against Twitch
-Invoke-RestMethod -Uri 'https://id.twitch.tv/oauth2/validate' -Headers @{ Authorization = "OAuth $tokClean" } -ErrorAction Stop
